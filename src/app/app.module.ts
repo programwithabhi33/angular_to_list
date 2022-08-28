3564ae86613b2e83cf0e7da8f6812c4197b99a36
program_with_abhi <programwithabhi@gmail.com>
2022-08-28 14:53:44 +0530

    Add new component in the angula

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './myComponent/todos/todos.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
+import { AbhiShekComponent } from './abhi-shek/abhi-shek.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    SecondComponentComponent,
+    AbhiShekComponent
   ],
   imports: [
     BrowserModule,

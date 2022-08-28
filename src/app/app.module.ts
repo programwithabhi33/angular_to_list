02aabac833bfd5d04d402f5edae7c3d575939491
program_with_abhi <programwithabhi@gmail.com>
2022-08-28 15:39:44 +0530

    Added the appcomponent in the module.ts removed by mistakely

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './myComponent/todos/todos.component';
+// import { TodosComponent } from './myComponent/todos/todos.component';
 import { SecondComponentComponent } from './myComponent/second-component/second-component.component';
 
 
 @NgModule({
   declarations: [
+    AppComponent,
     TodosComponent,
     SecondComponentComponent
   ],
